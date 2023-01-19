ede2e2c63c28443990d1a46dbf9cf278f1154927
Benny Kremer <benjamin.kremer1@outlook.de>
2023-01-19 17:36:23 +0100

    add clouds

---
+++ models/clouds.class.js
@@ -0,0 +1,8 @@
+class Cloud extends MovableObject {
+
+    constructor() {
+        super().loadImage('img/5_background/layers/4_clouds/1.png');
+
+        this.x = 200 + Math.random() * 500; //Zahl zwischen 200 und 700
+    }
+}
