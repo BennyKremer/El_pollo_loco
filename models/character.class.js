8d86e4d10714884cdeef5cf8652a6dbc5815066c
Benny Kremer <benjamin.kremer1@outlook.de>
2023-01-23 22:14:51 +0100

    add Endboss

--- models/character.class.js
+++ models/character.class.js
@@ -12,6 +12,8 @@ class Character extends MovableObject {
     "img/2_character_pepe/2_walk/W-26.png",
   ];
   world;
+  walking_sound = new Audio('audio/running.mp3');
+
 
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
@@ -23,28 +25,30 @@ class Character extends MovableObject {
   animate() {
 
     setInterval(() => {
-        if (this.world.keyboard.RIGHT) {
+        this.walking_sound.pause();
+        if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
             this.x += this.speed;
             this.otherDirection = false;
+            this.walking_sound.play();
         }
 
-        if (this.world.keyboard.LEFT) {
+        if (this.world.keyboard.LEFT && this.x > 0) {
             this.x -= this.speed;
             this.otherDirection = true;
+            this.walking_sound.play();
         }
-        this.world.camera_x = -this.x;
+        this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
 
     setInterval(() => {
       if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
         //Walk animation
-        let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; => // i = 0, 1, 2, 3, 4, 5, 0,...
-        let path = this.IMAGES_WALKING[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
+        this.playAnimation(this.IMAGES_WALKING);
       }
     }, 50);
   }
 
-  jump() {}
+  jump() {
+    
+  }
 }
